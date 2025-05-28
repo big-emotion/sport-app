20559301c8ff1522bfae0165c05991046a8b8ddd
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-05-28 17:27:33 +0200

    fix: fix lint

--- frontend-web/src/app/[locale]/[slug]/page.tsx
+++ frontend-web/src/app/[locale]/[slug]/page.tsx
@@ -1,23 +1,16 @@
 import { notFound } from 'next/navigation';
 import MapInteractive from '@/app/components/map/MapContainer';
-
-type Props = {
-  params: {
-    locale: string;
-    slug: string;
-  };
-};
-
-export default function LocalizedPage({ params }: Props) {
-  const { locale, slug } = params;
-
+export default async function LocalizedPage({
+                                              params,
+                                            }: {
+  params: Promise<{ locale: string; slug: string }>;
+}) {
+  const { locale, slug } = await params;
   const isMap =
     (locale === 'fr' && slug === 'carte-interactive') ||
     (locale === 'en' && slug === 'map-interactive');
-
   if (isMap) {
     return <MapInteractive />;
   }
-
   return notFound();
 }
