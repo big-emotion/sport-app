54ff079c3af0df54f922bba3a343a2e1eef777e5
Fabio Jorge <fabio.jorge@big-emotion.com>
2025-07-02 14:55:49 +0200

    feat: middleware

---
+++ frontend-web/src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest): NextResponse | void {
+  console.log('Middleware triggered for:', request.nextUrl.pathname);
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
